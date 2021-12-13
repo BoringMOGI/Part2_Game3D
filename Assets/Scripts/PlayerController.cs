948a225699b700a66ad29dd21db0c3055ec09bf1
Seungmok <ksw617@gmail.com>
2021-12-13 16:05:35 +0900

    Step3 - Attackable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public interface IDamageable
+{
+    void OnDamaged(float amount);
+}
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Animator anim;
@@ -11,27 +16,64 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float attackRadius;
     [SerializeField] LayerMask attackMask;
 
+    [Header("Etc")]
+    [SerializeField] float comboDelay;
+
+    Coroutine comboReset;
+
     bool isAttack;          // 공격 중인가?
+    int combo
+    {
+        get
+        {
+            return anim.GetInteger("combo");
+        }
+        set
+        {
+            anim.SetInteger("combo", value);
+        }
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;       // 마우스 잠그기.
+        Cursor.visible = false;                         // 마우스 포인터 비활성화.
+    }
 
     void Update()
     {
         // GetMouseButtonDown(0:왼쪽, 1:오른쪽, 2:휠)
-        if(Input.GetMouseButtonDown(0) && !isAttack)
+        if(Input.GetMouseButtonDown(0) && !isAttack && combo < 3)
         {
-            isAttack = true;
-            anim.SetTrigger("onAttack");
+            Attack();
         }
     }
 
+    void Attack()
+    {
+        // 연속 공격 타이머.
+        if (comboReset != null)
+            StopCoroutine(comboReset);
+
+        // 변수 지정.
+        isAttack = true;
+        combo += 1;
+        anim.SetTrigger("onAttack");
+    }
     public void OnHit()
     {
-        Collider[] hits = Physics.OverlapSphere(attackPivot.position, attackRadius, attackMask);
-        foreach(Collider hit in hits)
-        {
-            Debug.Log(hit.gameObject.name);
-        }
-
+        comboReset = StartCoroutine(ComboReset());
         OnEndAttack();
+
+        Collider[] hits = Physics.OverlapSphere(attackPivot.position, attackRadius, attackMask);
+        foreach (Collider hit in hits)
+        {
+            IDamageable target = hit.GetComponent<IDamageable>();
+            if(target != null)
+            {
+                target.OnDamaged(1);
+            }
+        }
     }
 
     public void OnEndAttack()
@@ -39,7 +81,21 @@ public class PlayerController : MonoBehaviour
         isAttack = false;
     }
 
-    private void OnDrawGizmos()
+    IEnumerator ComboReset()
+    {
+        float comboTime = comboDelay;                       // 연속 공격 시간.
+
+        while ((comboTime -= Time.deltaTime) > 0.0f)       // 시간이 다 되었다면.
+            yield return null;
+
+        combo = 0;                                          // 콤보를 초기화.
+    }
+
+
+
+
+
+private void OnDrawGizmos()
     {
         if(attackPivot != null)
         {
---
+++ Assets/Scripts/SandBag.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandBag : MonoBehaviour, IDamageable
+{
+
+    void IDamageable.OnDamaged(float amount)
+    {
+        StartCoroutine(DamageEffect());
+    }
+
+
+    IEnumerator DamageEffect()
+    {
+        Material mainMat = GetComponent<MeshRenderer>().material;       // MeshRenderer의 메인 머티리얼.
+
+        Color originColor = mainMat.color;                              // 원래 가지고있던 색을 담는다.
+        mainMat.color = Color.red;                                      // 색상을 빨강으로 변경.
+
+        yield return new WaitForSeconds(0.05f);                          // 0.2초 기다림.
+
+        mainMat.color = originColor;                                    // 원래의 색으로 변경.
+    }
+}
